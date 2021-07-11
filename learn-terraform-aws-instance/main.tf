ff07e83838260a736a32684b2cd23c4f273ed97e
AlessandraMayumi <alessandra_mms@hotmail.com>
2021-07-11 10:09:56 -0300

    first commit

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,23 @@
+terraform {  
+  required_providers {    
+    aws = {      
+      source  = "hashicorp/aws"      
+      version = "~> 3.27"    
+    }  
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0dc2d3e4c0f9ebd18"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
